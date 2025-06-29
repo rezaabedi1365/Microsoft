713ace4973a62da03302c04b92790ae1a64adfaa
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2025-06-29 09:24:02 +0330

    Update Remove or Disable lastlogin30Day users.ps1

--- Active Directory/UserManagement/Remove or Disable lastlogin30Day users.ps1
+++ Active Directory/UserManagement/Remove or Disable lastlogin30Day users.ps1
@@ -1,5 +1,11 @@
+
 #Find
 Search-ADAccount -AccountInactive -ComputersOnly -TimeSpan 90.00:00:00 | Select-Object Name, LastLogonDate
+     #only last90 Days login Active user 
+Search-ADAccount -AccountInactive -ComputersOnly -TimeSpan 90.00:00:00 |
+Where-Object { $_.Enabled -eq $true } |
+Select-Object Name, LastLogonDate
+
 
 #Report
 Search-ADAccount -AccountInactive -ComputersOnly -TimeSpan 90.00:00:00 |
